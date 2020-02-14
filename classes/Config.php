c6f552c7fdc003972b0fb745cefadc0a3de8eb08
Gianni <gianni.brouwers@student.kdg.be>
2020-02-14 20:15:30 +0100

    Optie toegevoegd om zelf een keyword te kiezen

--- classes/Config.php
+++ classes/Config.php
@@ -10,28 +10,15 @@
 class Config
 {
     public $Alphabet;
-    protected  $keyWord;
+    protected $keyWord;
 
     public function __construct(){
         // get all the letters needed for the game.
         $this->Alphabet = range('A', 'Z');
-        // Generate a new keyword
-        $this->GenerateKeyWord();
     }
 
-    public function GenerateKeyWord(){
-        // explode — Split a string by a string
-        // The file_get_contents() reads a file into a string.
-        // leest heel de file in een lange string en splits deze op in array elementen:
-        // @info LET OP alle woorden in de file moeten in lowercase!
-        $keyWordValues = explode(",", file_get_contents('"./assets/woordenlijst_nl.txt'));
-        // Deleting last array item to get rid of whitespace.
-        array_pop($keyWordValues);
-        // Genereer een random woord:
-        $max = count($keyWordValues) - 1;
-        $rndIdx = rand(0,$max);
-        // assign keyWord value:
-        $this->keyWord = $keyWordValues[$rndIdx];
+    public function SetKeyWord($key){
+         $this->keyWord = $key;
     }
 
     public function GetKeyWord(){
--- startscherm.php
+++ startscherm.php
@@ -27,12 +27,19 @@ if (isset($_POST['Opnieuw'])){
       <div id="wrapper">
             <h1>hangman</h1>
     <form method="post" action="index.php">
+        <div class="">
+            <label for="keyword">Pick your keyword:
+                <input type="password" name="keyword" autocomplete="off" required>
+            </label>
+        </div>
+
     <label for="difficulty">Select difficulty:
         <select id="difficulty" name="difficulty">
             <option selected value="10">Easy</option>
             <option value="7"> Hard</option>
         </select>
     </label>
+
         <h3><button type="submit" name="startGame">Start Game</button></h3>
     </form>
   </div>
